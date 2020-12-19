93f1bdfaccc65a928492bc8ccf93379e77e58727
Abdur Rahman <khalil.ahmad1547@gmail.com>
2020-12-19 09:36:30 +0500

    M_C & M_P CEP's code and Schematics testing.
    I have done som e code as till now we don't know much we are still
    testing it how to use it properly.

--- Code/Micro_CEP.c
+++ Code/Micro_CEP.c
@@ -1,10 +1,14 @@
 
 void main() {
-
+     TrisD = 0;
+     LatD = 0b00000101;
      PWM1_Init(10000);
-     
-     PWM1_Set_Duty(229);
-     
+     PWM1_Set_Duty(255);
      PWM1_Start();
-
+     PWM2_Init(10000);
+     PWM2_Set_Duty(255);
+     PWM2_Start();
+     while(1)
+     {
+     }
 }
