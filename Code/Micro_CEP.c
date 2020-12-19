91604eeb26af1b2e6ca0ef87a99b833cc20809ec
Abdur Rahman <khalil.ahmad1547@gmail.com>
2020-12-19 13:31:21 +0500

    Some Parameter are exchnaged to avoid confusion.
    In Sensers:
    Low value PINs are toword Right Now
    High Value PINs are toword Left Now
    
    Middel Remain Same.
    
    IN Motors:
    Left Motors and Right Motors are exchanged.
    
    IN1 & IN 2 are for Motor 1 &
    IN2 & IN 3 are for Motor 2

--- Code/Micro_CEP.c
+++ Code/Micro_CEP.c
@@ -1,6 +1,6 @@
 // Enable Pins of H-Bridge
-#define Enable_Pin_One LatC.F1  // EN1 connected with PortC.1
-#define Enable_Pin_Two LatC.F2  // EN2 connected with PortC.2
+#define Enable_Pin_One LatC.F1      // EN1 connected with PortC.1
+#define Enable_Pin_Two LatC.F2      // EN2 connected with PortC.2
 
 // Input Pins of H-Bridge
 // For 1st Motor
@@ -12,11 +12,11 @@
 
 // Sensers Pins
 #define Senser_One PortB.F0
-#define Senser_Left PortB.F1        // Left Senser
+#define Senser_Right PortB.F1        // Left Senser
 #define Senser_Three PortB.F2
 #define Senser_Head PortB.F3        // Head Sense
 #define Senser_Five PortB.F4
-#define Senser_Right PortB.F5       // Right Senser
+#define Senser_Left PortB.F5       // Right Senser
 
 // Snesers default Ouput Value
 #define is_Line    1
@@ -30,10 +30,10 @@ void Motor_Right_Stop();
 void Motor_Right_Start();
 
 void main() {
-     TrisD = 0;                 // making D port OUTPUT
-     TrisC = 0;                 // Making C port OUTPUT
+     TrisD = 0;                     // making D port OUTPUT
+     TrisC = 0;                     // Making C port OUTPUT
      
-     TrisB = 0xff;              // Making Port B INPUT
+     TrisB = 0xff;                  // Making Port B INPUT
      // by default running staraight
      Input_Pin_One = 0;
      Input_Pin_Two = 1;
@@ -46,23 +46,29 @@ void main() {
              if(Senser_Head == not_Line)
              {
                   if(Senser_Left == not_Line && Senser_Right == not_Line)
-              {
-                 // Move straight
-                 Motor_Left_Start();
-                 Motor_Right_Start();
-              }
-             else if (Senser_Left == is_Line && Senser_Right == not_Line)
-              {
-               // Turn Left
-               Motor_Left_Stop();
-               Motor_Right_Start();
-              }
-             else if (Senser_Left == not_Line && Senser_Right == is_Line)
-              {
-                  // Turn Left
-                  Motor_Left_Start();
-                  Motor_Right_Stop();
-              }
+                     {
+                          // Dead END here
+                          Motor_Left_Stop();
+                          Motor_Right_Start();
+                     }
+                  else if (Senser_Left == is_Line && Senser_Right == not_Line)
+                      {
+                          // Turn Left
+                          Motor_Left_Stop();
+                          Motor_Right_Start();
+                      }
+                  else if (Senser_Left == not_Line && Senser_Right == is_Line)
+                       {
+                          // Turn Right
+                          Motor_Left_Start();
+                          Motor_Right_Stop();
+                       }
+                  else if (Senser_Left == is_Line && Senser_Right == is_Line)
+                       {
+                          // Turn Right
+                          Motor_Left_Start();
+                          Motor_Right_Stop();
+                       }
              }
              else
              {
@@ -73,25 +79,25 @@ void main() {
 }
 
 // Function Definations
-void Motor_Left_Stop()
+void Motor_Right_Stop()
 {
     Input_Pin_One = 0;
     Input_Pin_Two = 0;
 }
 
-void Motor_Left_Start()
+void Motor_Right_Start()
 {
     Input_Pin_One = 1;
     Input_Pin_Two = 0;
 }
 
-void Motor_Right_Stop()
+void Motor_Left_Stop()
 {
     Input_Pin_Three = 0;
     Input_Pin_Four = 0;
 }
 
-void Motor_Right_Start()
+void Motor_Left_Start()
 {
     Input_Pin_Three = 1;
     Input_Pin_Four = 0;
