2d6fc74e2e0ca1cc77177cd2b33f2e2552d129a9
Abdur Rahman <khalil.ahmad1547@gmail.com>
2020-12-19 10:14:19 +0500

    Sensers Pins are added in Code file
    Sensers which are going to be used are defined in code.

--- Code/Micro_CEP.c
+++ Code/Micro_CEP.c
@@ -3,13 +3,22 @@
 #define Enable_Pin_Two LatC.F2  // EN2 connected with PortC.2
 
 // Input Pins of H-Bridge
+// For 1st Motor
 #define Input_Pin_One LatD.F0       // IN1 connected to PortD.0
 #define Input_Pin_Two LatD.F1       // IN2 connected to PortD.1
+// For 2nd Motor
 #define Input_Pin_Three LatD.F2     // IN3 connected to PortD.2
 #define Input_Pin_Four LatD.F3      // IN4 connected to PortD.3
 
 // Sensers Pins
+#define Senser_One PortB.F0
+#define Senser_Two PortB.F1
+#define Senser_Three PortB.F2
+#define Senser_Four PortB.F3
+#define Senser_Five PortB.F4
+#define Senser_Six PortB.F5
 
+// Motor controlling Functions
 
 void main() {
      TrisD = 0;                 // making D port OUTPUT
